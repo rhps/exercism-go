850ca1def2dbf06b656cfcabedd4a9a24ecbd1f0
Rio Harapan Ps <github@harapan.me>
2022-09-07 01:03:20 +0100

    solved annalyns

--- annalyns-infiltration/annalyns_infiltration.go
+++ annalyns-infiltration/annalyns_infiltration.go
@@ -11,15 +11,28 @@ func CanFastAttack(knightIsAwake bool) bool {
 
 // CanSpy can be executed if at least one of the characters is awake.
 func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
+	var canSpying = false
+	if knightIsAwake || archerIsAwake || prisonerIsAwake {
+		canSpying = true
+	}
+	return canSpying
 }
 
 // CanSignalPrisoner can be executed if the prisoner is awake and the archer is sleeping.
 func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
-	panic("Please implement the CanSignalPrisoner() function")
+	var canSignal = false
+	if prisonerIsAwake && !archerIsAwake {
+		canSignal = true
+	}
+	return canSignal
 }
 
 // CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
 // or if Annalyn's pet dog is with her and the archer is sleeping.
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
-	panic("Please implement the CanFreePrisoner() function")
+	var freePrisoner = false
+	if (prisonerIsAwake && (!knightIsAwake && !archerIsAwake)) || (petDogIsPresent && !archerIsAwake) {
+		freePrisoner = true
+	}
+	return freePrisoner
 }
